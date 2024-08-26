e138d7892ed4a26769af1105b6429c0e5c84d156
Auriel James <aurieljames11@gmail.com>
2024-08-26 12:22:22 +0800

    Update Functionality Done

--- components/Removebtn.tsx
+++ components/Removebtn.tsx
@@ -2,14 +2,15 @@ import { HiOutlineTrash } from "react-icons/hi";
 import { removeToList } from "@/actions/todoActions";
 
 export default function Removebtn(props:any){
-    const handleRemoveClick = async() => {
+    const handleRemoveClick = async(event:any) => {
+        event.stopPropagation();
         console.log(props.id);
         await removeToList(props.id);
         const getAllTask =  props.func;
         await getAllTask;
     }
     return(
-        <button className="text-red-600" onClick={handleRemoveClick}>
+        <button className="z-50" onClick={handleRemoveClick}>
             <HiOutlineTrash size={24}/>
         </button>
     )
